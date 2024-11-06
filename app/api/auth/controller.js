c40ff2fd296e97f7d3c1f20095b85b684066f8a1
Sofa <muhammadsofa52@yahoo.com>
2024-11-06 23:14:33 +0700

    Sign in

---
+++ app/api/auth/controller.js
@@ -0,0 +1,34 @@
+const {User} = require('../../db/models');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+     signin : async(req, res, next)=>{
+          try {
+               const{email, password} = req.body;
+               const checkUser = await User.findOne({where: {email}});
+
+               if(checkUser){
+                    const checkPassword = bcrypt.compareSync(password, checkUser.password);
+
+                    if(checkPassword){
+                         const token = jwt.sign({
+                              user: {
+                                   id: checkUser.id,
+                                   name: checkUser.name,
+                                   email: checkUser.email,
+                              }
+                         }, 'secret');
+                         res.status(200).json({message: 'Success Signin', data: token})
+                    } else {
+                         res.status(403).json({message: 'Invalid Password'})
+                    }
+               }else{
+                    res.status(403).json({message: 'Invalid Email'})
+               }
+          } catch (error) {
+               console.log(error);
+               next(error);
+          }
+     }
+}
